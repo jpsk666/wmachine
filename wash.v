cdf5259aa8302405e11b8438b6bbfa78797ff5ac
zhuyucheng30428 <tony30428@gmail.com>
2022-12-20 00:48:38 +0800

    zhuyuc

--- wash.v
+++ wash.v
@@ -6,11 +6,14 @@ module wash (
     output [3:0] ena,  //数码管使能信号
     output reg [7:0] st_light //接灯
     )
-
-reg [33:0]t;//计时100秒
-reg [3:0] n1, n2, n3, n0; //1~3：个位至百位; 0:符号位
-parameter o = 1'b0;
-reg [1:0] state = 1'b0;  //3种状态
+parameter o = 1'b0;//显示0
+parameter n = 4'd11;//熄灯
+reg [26:0]t;//计时1秒
+reg [3:0] n1=o;//数码管初始化
+reg [3:0] n2=n;
+reg [3:0] n3=4'd9;
+reg [3:0] n0=4'd9;
+reg [1:0] st = 1'b0;  //3种状态
 
 scan4 scanner (
       clk,
@@ -38,45 +41,58 @@ always @(*) begin//小灯
     endcase
   end
 always @(posedge clk, negedge rst) begin
-    
     if (!rst) begin
-      st <= 1'b0;
-      {n1,n2,n3,n0}={o,o,o,o};
+        st <= 1'b0;
+        {n1,n2,n3,n0}={o,n,4'd9,4'd9};
     end 
     else begin
-      if(on) begin
-      case (st)  //状态判断
-        2'b0: begin
-          if (t >= 66000000) begin
-            t <= 0;
-            if (p1) begin  //拨上去时才加1,是否要写拨回去时的分支？
-              if (n1 != 4'd9) n1 <= n1 + 1;
-              else n1 <= 0;
-            end else n1 <= n1;
-            if (p2) begin
-              if (n2 != 4'd9) n2 <= n2 + 1;
-              else n2 <= 0;
-            end else n2 <= n2;
-            if (p3) begin
-              if (n3 != 4'd9) n3 <= n3 + 1;
-              else n3 <= 0;
-            end else n3 <= n3;
-            if (sign) begin
-              if (n0 != 4'd10) n0 <= 4'd10;  //10代表负号
-              else n0 <= 0;
-            end else n0 <= n0;
-          end else t <= t + 1;
+        if(on) begin
+            if (t >= 100000000) begin //降频到1秒
+                t <= 0;
+                if (n3 != 4'd0) begin
+                    n0 <= n0;
+                    n3 <= n3 - 1;//倒计时100秒
+                end
+                else begin
+                    if(n0!= 4'd0) begin
+                        n3 <= 9;
+                        n0 <= n0 - 1;
+                    end
+                    else begin
+                        n3 <= n3;
+                        n0 <= n0
+                    end
+                end
+            end 
+            else begin 
+                t <= t + 1;
+            end
+
+            case (st)  //放水脱水阶段
+                2'b0: begin
+                    if (t >= 100000000) begin
+                        if(n0==4'd9 && n3<9) n1 <= n1 + 1;
+                        else begin
+                            n1 <= n1;
+                            st <= st + 1;
+                        end
+                    end
+                end
+                2'b01: begin
+                    if (n0<=3) st <= st + 1;
+                end
+                2'b10: begin
+                    if (t >= 100000000) begin
+                        if(n3==4'd9 && n3>0) n1 <= n1 - 1;
+                        else begin
+                            n1 <= n1;
+                            st <= st;
+                        end
+                    end
+                end
+            endcase
         end
-        2'b01: begin
-          {n2, n3, n0} <= {o, o, o};
-          if (r_pos)
-            if (n1 < 4'd3) n1 <= n1 + 1;
-            else n1 <= 0;
-          else n1 <= n1;
-        end
-      endcase
     end
-    end
-  end
+end
 
 endmodule
