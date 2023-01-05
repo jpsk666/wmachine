f125adc249d25c1ad3649c1844970d23d85948fe
Autoluminous <2298195710@qq.com>
2023-01-05 21:08:11 +0800

    working

--- billing.v
+++ billing.v
@@ -4,11 +4,15 @@ module billing (
     input on, clk,rst,
     input [11:0] bal,
     input [1:0] mode,
+    input [11:0] set0,//传入甩干价格
+    input [11:0] set1,//小
+    input [11:0] set2,//中
+    input [11:0] set3,//大
     output wire [7:0] led,  //数码管信号
     output [3:0] ena,  //数码管使能信号
     output reg [7:0] st_light //接灯
     );
-parameter o = 1'b0;//显示0
+parameter o = 4'd0;//显示0
 parameter n = 4'd11;//熄灯
 reg [26:0]t;//计时1秒
 reg [3:0] n1=o;//数码管初始化
@@ -16,6 +20,7 @@ reg [3:0] n2=n;
 reg [3:0] n3=4'd9;
 reg [3:0] n0=4'd9;
 reg st = 0;  //2种状态
+reg [15:0]setini={n,n,n,n};
 
 scan4 scanner (
       clk,
@@ -44,17 +49,17 @@ end
 always @(posedge clk, negedge rst) begin
     if (!rst) begin
         st <= 1'b0;
-        {n1,n2,n3,n0}<={o,n,4'd9,4'd9};
+        {n1,n2,n3,n0}<=setini;
     end 
     else begin
         if(on) begin
             if(st==1'b0)begin
                 case(mode)
                     2'b00:begin//甩干
-                        
+                        setini<={set0,n};
                     end
                     2'b01:begin//小
-                        
+                        setini<={set1,n};
                     end
                     2'b10:begin//中
                         
